e814e921d851e68cc26a13e4e87584d28f860c0c
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-03-29 03:53:19 +0900

    引数とjsonの読み込み

--- size_from_single_json.py
+++ size_from_single_json.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
+import sys
+import argparse
+import json
 
 def main():
-    print("main")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("FILE_Input", help="Set File Path", type=argparse.FileType('r'))
+    args = parser.parse_args()
 
+    json.load(args.FILE_Input)
+
+    args.FILE_Input.close()
+    
 # main関数呼び出し
 if __name__ == "__main__":
     main() 
