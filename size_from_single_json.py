6d1df7f82aeebf3c7bddfda700f13e71ad927413
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-03-29 11:08:20 +0900

    JSONのイテレーションと判定

--- size_from_single_json.py
+++ size_from_single_json.py
@@ -8,10 +8,15 @@ def main():
     parser.add_argument("FILE_Input", help="Set File Path", type=argparse.FileType('r'))
     args = parser.parse_args()
 
-    json.load(args.FILE_Input)
+    json_dict = json.load(args.FILE_Input)
+    
+    # 投稿ごとにループ
+    for msg in json_dict:
+        print(msg['type'])
+        print('files' in msg)
 
     args.FILE_Input.close()
     
 # main関数呼び出し
 if __name__ == "__main__":
-    main() 
+    main()
