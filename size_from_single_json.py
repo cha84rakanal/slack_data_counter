2ae8a0e25a04c9f9125ce37e3d9eaec809ae66cf
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-03-29 12:00:10 +0900

    1チャンネル1日のトータルファイルサイズが取得できるようになった

--- size_from_single_json.py
+++ size_from_single_json.py
@@ -9,11 +9,26 @@ def main():
     args = parser.parse_args()
 
     json_dict = json.load(args.FILE_Input)
+
+    total_size = 0
     
     # 投稿ごとにループ
     for msg in json_dict:
-        print(msg['type'])
-        print('files' in msg)
+        if msg['type'] == "message":
+            if 'hidden' in msg:
+                if msg['hidden'] == True:
+                    continue
+
+            if 'subtype' in msg:
+                if msg['subtype'] == "message_changed":
+                    continue
+            
+            if 'files' in msg:
+                for f in msg['files']:
+                    print(f['name'] + " " + str(f['size']) + " byte")
+                    total_size = total_size + f['size']
+
+    print("total file size: " + str(total_size) + " byte")
 
     args.FILE_Input.close()
     
