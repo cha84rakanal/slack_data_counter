b7aaf075ad13954720044b16523e441ad2058c2a
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-03-29 21:58:52 +0900

    ファイルの一覧を取得

--- size_from_directory.py
+++ size_from_directory.py
@@ -14,7 +14,12 @@ def main():
 
     files = os.listdir(path)
     files_dir = [f for f in files if os.path.isdir(os.path.join(path, f))]
-    print(files_dir) 
+
+    # ディレクトリごとの処理
+    for dir_i in files_dir:
+        files = os.listdir(path+dir_i+"/")
+        for file_i in files:
+            print(path+dir_i+"/"+file_i)
 
 # main関数呼び出し
 if __name__ == "__main__":
